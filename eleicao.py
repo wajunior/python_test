fa5fea0efc360ffc224cd36d1babb3477dbdeb25
WilsonJunior <wilson_junior1994@hotmail.com>
2022-06-07 15:34:49 -0300

    Updated Commit

--- eleicao.py
+++ eleicao.py
@@ -1,28 +1,33 @@
-candidatoA = 0
-candidatoB = 0
-candidatoC = 0
+# Setando variáveis
+candidatoA = candidatoB = candidatoC = 0
 cont = 1
 
 print('=' * 50)
 print('{:^50}'.format('Eleições 2022'))
 print('=' * 50)
 
+# Inicia as votações
 while cont != 0:
     op = str(input('Gostaria de iniciar as votações? [S/N]: ')).lower()
 
+    # Inicia as votações se a opção for S
     if op == 's':
         c = 1
 
+        # Informa a quantidade de eleitores
         while c != 0:
             eleitores = int(input('Informe a quantidade de eleitores: '))
             c += 1
 
+            # Começa o laço com a quantidade de eleitores informados
             for n in range(1, eleitores + 1):
                 print('======== ELEITOR Nº {} ========'.format(n))
                 votos = int(input('[1] - Bolsonaro\n'
                                   '[2] - Lula\n'
                                   '[3] - Ciro Gomes\n'
                                   ': '))
+
+                # Computa quantos votos cada candidato teve
                 if votos == 1:
                     candidatoA += 1
                 elif votos == 2:
@@ -32,13 +37,18 @@ while cont != 0:
                 else:
                     print('Valor incorreto!')
 
-            opc = str(input('Gostaria de continuar? [S/N]: ')).lower()
+            # Pergunta se continua, para ou zera o resultado
+            opc = str(input('Gostaria de continuar ou zerar resultados? [S/N/R(zerar)]: ')).lower()
 
-            if opc != 's':
+            # Ação de sair do programa
+            if opc == 'n':
                 print('Saindo do programa!')
                 c = 0
                 cont = 0
-
+            # Ação de zerar os votos informados anteriormente
+            elif opc == 'r':
+                votos = 0
+                candidatoA = candidatoB = candidatoC = 0
     else:
         print('Saindo do programa!')
         cont = 0
